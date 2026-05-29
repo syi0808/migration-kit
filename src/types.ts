53b244f7840a6da5ea6433945fd3659694a51adb
syi0808 <syi9397@naver.com>
2026-05-29 19:57:09 +0900

    feat: add interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,39 @@
-export {};
+export interface MigrationRunnerOptions {
+  name: string;
+  from: string;
+  to: string;
+  docs?: string;
+  configPath?: string[];
+  environment?: EnvironmentRequirementCheck[];
+  peerDependencies?: PeerDependency[];
+  configChanges?: ConfigChange[];
+  apiChanges?: ApiChange[];
+}
+
+export type EnvironmentAvailableStatus = boolean;
+
+export type EnvironmentRequirementCheck = () => EnvironmentAvailableStatus;
+
+export interface PeerDependency {
+  dependency: string;
+  requiredVersion: string;
+}
+
+export interface ConfigChange {
+  title: string;
+  description?: string;
+  level: "error" | "warning";
+  shouldBlock?: (configPath: string) => false | { reason: string };
+  transform?: Transformer;
+}
+
+export interface ApiChange {
+  title: string;
+  description?: string;
+  files: string[];
+  shouldBlock?: (configPath: string) => false | { reason: string };
+  transform?: Transformer;
+}
+
+export type TransformedCode = string;
+export type Transformer = (filePath: string) => TransformedCode;
