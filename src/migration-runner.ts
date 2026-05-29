c73e52ca5edba78def38367598dd5329211b44d3
syi0808 <syi9397@naver.com>
2026-05-29 23:03:30 +0900

    feat: migration runner

--- src/migration-runner.ts
+++ src/migration-runner.ts
@@ -1,3 +1,52 @@
-function createMigrationRunner() {}
+import type { MigrationRunnerOptions } from "./types.js";
+import { createLogUpdate } from "log-update";
+
+const logUpdate = createLogUpdate(process.stdout);
+
+function createMigrationRunner(options: MigrationRunnerOptions): void {
+  const {
+    name,
+    from,
+    to,
+    docs,
+    configPath,
+    environment,
+    peerDependencies,
+    apiChanges,
+    configChanges,
+  } = options;
+
+  logUpdate.persist(`${name} (${from} -> ${to})`);
+
+  if (docs) {
+    logUpdate.persist(`Docs: ${docs}`);
+  }
+
+  if (environment && environment.length > 0) {
+    logUpdate.persist("Environment");
+
+    // ...
+  }
+
+  if (peerDependencies && peerDependencies.length > 0) {
+    logUpdate.persist("Dependencies");
+
+    // ...
+  }
+
+  if (configChanges && configChanges.length > 0) {
+    logUpdate.persist("Config Changes");
+
+    // ...
+  }
+
+  if (apiChanges && apiChanges.length < 0) {
+    logUpdate.persist("API Changes");
+
+    // ...
+  }
+
+  logUpdate.persist(`${name} Completed!`);
+}
 
 export { createMigrationRunner };
